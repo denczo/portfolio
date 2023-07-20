adc1d788cd38d79e47dc34473c6d2d1933240f07
denczo <d.czogalla@gmx.de>
2023-07-20 21:16:43 +1000

    EDIT: adjusted styling work section

--- frontend/src/structure/Work.tsx
+++ frontend/src/structure/Work.tsx
@@ -8,13 +8,13 @@ const Work = () => {
       {/* <div className="text-white text-xl font-bold mb-8 text-center">EXPERIENCE</div> */}
       {data.reverse().map((item, index) => <div key={"W"+index} className="flex max-w-3xl">
           <div>
-            <div className="flex justify-center items-center text-white rounded-full w-24 h-24  bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
-              <div className="flex h-[90px] w-[90px] bg-slate-800 rounded-full justify-center items-center"><b>{item.year.toString()}</b></div>
+            <div className="flex justify-center items-center text-white rounded-full desktop:w-24 mobile:mt-6 desktop:h-24 mobile:w-12 mobile:h-12 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+              <div className="flex desktop:h-[90px] desktop:w-[90px] mobile:h-[45px] mobile:w-[45px] bg-slate-800 desktop:font-bold rounded-full justify-center items-center">{item.year.toString()}</div>
             </div>
             {index < data.length - 1 ? 
-              <div className="w-1 bg-slate-900 m-auto h-full"></div> : <div className="h-16"></div>}
+              <div className="mobile:w-6 desktop:w-2 bg-slate-900 m-auto h-full"></div> : <div className="h-16"></div>}
           </div>
-          <div className="flex flex-col justify-start h-max mt-9 pl-6 text-white">
+          <div className="flex flex-col justify-start h-max mt-9 desktop:pl-6 mobile:pl-4 text-white">
             <div><b>{item.title + " @ " + item.company}</b></div>
             <div>{item.tasks}</div>
             <ul className="mt-1 flex flex-wrap" aria-label="Technologies used:">
