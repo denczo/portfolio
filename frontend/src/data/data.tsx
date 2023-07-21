56c765c91535c50dcd273512e98e29ae0d4c5048
denczo <d.czogalla@gmx.de>
2023-07-21 12:26:59 +1000

    EDIT: removed tag from data

--- frontend/src/data/data.tsx
+++ frontend/src/data/data.tsx
@@ -4,7 +4,7 @@ const data = [
     {title: "PyBlaster", image: "./content/PyBlaster.webp", link: "https://github.com/denczo/pyblaster", tags: ["Python","Tkinter","Matplotlib", "PyAudio", "DSP"], text: "Monophonic synthesizer which can be played with a midi keyboard. It's inspired by Korgs Kaossilator series."},
     {title: "FREM", image: "./content/KivyDesktop.webp", link: "https://github.com/denczo/FREM", tags: ["Python","Kivy","Android","Buildozer"], text: "An android app to explain and demonstrate the concepts of frequency modulation for dsp"},
     {title: "Arc Reactor", image: "./content/quarkreactor.webp", link: "https://github.com/denczo/arcreactor", tags: ["C","Blender","Cura"], text: "Illuminated Arc Reactor controlled by an Arduino Nano. It was modeled in Blender and 3d printed."},
-    {title: "Raspberry Bot", image: "./content/RaspiBot.webp", link: "https://github.com/denczo/RaspberryBot", tags: ["Python","Machine Learning","Raspberry Pi","Raspbian"], text: "A robot that uses reinforcement learning to find and follow a black line."},
+    {title: "Raspberry Bot", image: "./content/RaspiBot.webp", link: "https://github.com/denczo/RaspberryBot", tags: ["Python","Machine Learning","Raspberry Pi"], text: "A robot that uses reinforcement learning to find and follow a black line."},
 ]
 
 export default data;
