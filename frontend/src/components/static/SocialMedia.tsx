88e8238728f6fdd8827b1123cad1b61e1e892cdd
denczo <d.czogalla@gmx.de>
2024-03-14 18:27:58 +0100

    ADD: social media links

--- frontend/src/components/static/SocialMedia.tsx
+++ frontend/src/components/static/SocialMedia.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 
 const SocialMedia = ({size} : {size: number}) => {
     return (<div className="flex justify-center gap-10 pb-4">
-    <Image src="/icons/linkedin.svg" alt="Icon" width={size} height={size} />
-    <Image src="/icons/github.svg" alt="Icon" width={size} height={size} />
+    <a href="https://www.linkedin.com/in/denczo/"><Image src="/icons/linkedin.svg" alt="Icon" width={size} height={size} /></a>
+    <a href="https://github.com/denczo"><Image src="/icons/github.svg" alt="Icon" width={size} height={size} /></a>
     {/* <Image src="/icons/mail.svg" alt="Icon" width={size} height={size} /> */}
-    <Image src="/icons/youtube.svg" alt="Icon" width={size} height={size} />
+    <a href="https://www.youtube.com/@pygineer"><Image src="/icons/youtube.svg" alt="Icon" width={size} height={size} /></a>
   </div>)
 }
 
