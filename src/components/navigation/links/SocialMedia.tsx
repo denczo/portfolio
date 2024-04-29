bd9cb169ab92a4a7376772180983aec70525f7c0
denczo <d.czogalla@gmx.de>
2024-04-29 20:18:08 +0200

    FIX: another captcha fix

--- src/components/navigation/links/SocialMedia.tsx
+++ src/components/navigation/links/SocialMedia.tsx
@@ -7,10 +7,10 @@ const SocialMedia = ({size} : {size: number}) => {
 
     return (<div className="flex"  style={{ pointerEvents: 'auto' }}>
     <BounceIn delay={0.2}>
-    <a className="flex bg-black bg-opacity-50 p-4 mr-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.linkedin.com/in/denczo/"><Image src="/icons/linkedin.svg" alt="Icon" width={size} height={size} /></a>
-    <a className="flex bg-black bg-opacity-50 p-4 mx-1 rounded-full transition-all hover:scale-110 scale-100" href="https://github.com/denczo"><Image src="/icons/github.svg" alt="Icon" width={size} height={size} /></a>
-    <a className="flex bg-black bg-opacity-50 p-4 mx-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.youtube.com/@pygineer"><Image src="/icons/youtube.svg" alt="Icon" width={size} height={size} /></a>
-    <a className="flex bg-black bg-opacity-50 p-4 mx-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.instagram.com/thefullstacklab/"><Image src="/icons/insta.svg" alt="Icon" width={size} height={size} /></a>
+    <a className="flex bg-black bg-opacity-50 p-4 mr-1 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.linkedin.com/in/denczo/"><Image src="/icons/linkedin.svg" alt="Icon" width={size} height={size} /></a>
+    <a className="flex bg-black bg-opacity-50 p-4 mr-1 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://github.com/denczo"><Image src="/icons/github.svg" alt="Icon" width={size} height={size} /></a>
+    <a className="flex bg-black bg-opacity-50 p-4 mr-1 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.youtube.com/@pygineer"><Image src="/icons/youtube.svg" alt="Icon" width={size} height={size} /></a>
+    <a className="flex bg-black bg-opacity-50 p-4 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.instagram.com/thefullstacklab/"><Image src="/icons/insta.svg" alt="Icon" width={size} height={size} /></a>
     </BounceIn>
 
   </div>)
--- src/components/static/Contact.tsx
+++ src/components/static/Contact.tsx
@@ -1,27 +1,56 @@
-const Contact = () => {
+import { useForm } from "react-hook-form";
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+
+export default function Contact() {
+
+  const { register, handleSubmit, setValue } = useForm();
+
+  const onHCaptchaChange = (token: any) => {
+    setValue("h-captcha-response", token);
+  };
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+
+    await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: data
+    }).then((res) => res.json());
+  }
+
+
   return (
     <section className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 desktop:w-1/3 mobile:w-full mobile:px-4 mobile:max-w-3xl">
       <div className="px-2 mx-auto">
-        <form action="https://api.web3forms.com/submit" className="space-y-8" method="POST">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <input type="hidden" name="access_key" value="05c80678-e7a1-4da6-8ae9-1f09596fa3ef" />
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-white ">Your email</label>
             <input type="email" id="email" name="email" required className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="your@mail.com" />
           </div>
           <div>
-            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-white ">Subject</label>
+            <label htmlFor="subject" className="block my-2 text-sm font-medium text-white ">Subject</label>
             <input type="text" id="subject" name="subject" required className="block p-3 w-full text-sm text-gray-800 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Describe your subject" />
           </div>
           <div>
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">Your message</label>
+            <label htmlFor="message" className="block my-2 text-sm font-medium text-white">Your message</label>
             <textarea id="message" name="message" rows={6} className="block p-2.5 w-full text-sm text-gray-800   bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..."></textarea>
           </div>
-          <div className="h-captcha" data-captcha="true"></div>
-          <script src="https://web3forms.com/client/script.js" async defer></script>
-          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-gray-700 focus:outline-none scale-100 hover:scale-110 transition-transform">Send message</button>
+          <div className="py-4 desktop:hidden">
+            <HCaptcha
+              sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+              reCaptchaCompat={false}
+              size="compact"
+              onVerify={onHCaptchaChange} />
+          </div>
+          <div className="py-4 mobile:hidden">
+            <HCaptcha
+              sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+              reCaptchaCompat={false}
+              onVerify={onHCaptchaChange} />
+          </div>
+          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg border-white border-solid border-2 hover:bg-white hover:text-black transition-colors duration-300">Send message</button>
         </form>
       </div>
-    </section>)
+    </section>
+  )
 }
-
-export default Contact;
