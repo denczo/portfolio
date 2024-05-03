b33daf505c19c8173115efffa63726c9d6e4c77d
denczo <d.czogalla@gmx.de>
2024-05-03 14:27:26 +0200

    FIX: social media links

--- src/components/navigation/links/SocialMedia.tsx
+++ src/components/navigation/links/SocialMedia.tsx
@@ -9,7 +9,7 @@ const SocialMedia = ({size} : {size: number}) => {
     <BounceIn delay={0.2}>
     <a className="flex bg-black bg-opacity-50 p-4 mr-1 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.linkedin.com/in/denczo/"><Image src="/icons/linkedin.svg" alt="Icon" width={size} height={size} /></a>
     <a className="flex bg-black bg-opacity-50 p-4 mr-1 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://github.com/denczo"><Image src="/icons/github.svg" alt="Icon" width={size} height={size} /></a>
-    <a className="flex bg-black bg-opacity-50 p-4 mr-1 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.youtube.com/@pygineer"><Image src="/icons/youtube.svg" alt="Icon" width={size} height={size} /></a>
+    <a className="flex bg-black bg-opacity-50 p-4 mr-1 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.youtube.com/@fullstacklab"><Image src="/icons/youtube.svg" alt="Icon" width={size} height={size} /></a>
     <a className="flex bg-black bg-opacity-50 p-4 my-1 rounded-full transition-all hover:scale-110 scale-100" href="https://www.instagram.com/thefullstacklab/"><Image src="/icons/insta.svg" alt="Icon" width={size} height={size} /></a>
     </BounceIn>
 
