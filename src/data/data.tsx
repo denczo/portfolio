f8f75d9401c8decbc97686d877206dccd0d7cc63
denczo <d.czogalla@gmx.de>
2024-05-07 20:02:28 +0200

    FIX: typos

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -11,11 +11,11 @@ const data = [
     {title: "Fruitinator.org", image: frutinator, link: "https://www.fruitinator.org", tags: ["React.js","Javascript", "MobX", "SASS"], text: "A responsive e-commerce frontend for exotic fruit shopping on desktop and mobile"},
     {title: "Fullstacklab.net", image: portfolio, link: "https://www.fullstacklab.net", tags: ["Next.js","Typescript","Tailwind CSS"], text: "A single-page application with responsive design that highlights professional work and services"},
     {title: "Monomind.info", image: jblaster, link: "https://www.monomind.info", tags: ["React.js","Typescript","Web Audio API", "CSS"], text: "A monophonic synthesizer to play and record synth bass lines from the 80s"},
-    {title: "Taskify", image: taskify, link: "https://github.com/denczo/taskify", tags: ["React.js","Typescript","CSS","REST", "ExpressJS"], text: "A project managment tool to administer and keep track of ongoing tasks"},
+    {title: "Taskify", image: taskify, link: "https://github.com/denczo/taskify", tags: ["React.js","Typescript","CSS","REST", "Express.js"], text: "A project managment tool to administer and keep track of ongoing tasks"},
     {title: "FREM", image: frem, link: "https://github.com/denczo/FREM", tags: ["Python","Kivy","Android","Buildozer"], text: "An android app to explain and demonstrate the concepts of frequency modulation for dsp"},
-    {title: "Raspberry Bot", image: raspibot, link: "https://github.com/denczo/RaspberryBot", tags: ["Python","ML","Raspberry Pi","Tkinter"], text: "A self learning robot that uses Q-learning, a type of Machine Learning, to follow the line"},
-    {title: "PyBlaster", image: pyblaster, link: "https://github.com/denczo/pyblaster", tags: ["Python","PyAudio","Tkinter","RtMidi"], text: "A monophonic synthesizer built with Python that can be played with a Midi Keyboard"},
-    {title: "Arc Reactor", image: arc, link: "https://github.com/denczo/arcreactor", tags: ["3D Printing","Blender","C","Arduino"], text: "Using free Software Blender and Cura to model and 3D print the famous ARC reactor"},
+    {title: "Raspberry Bot", image: raspibot, link: "https://github.com/denczo/RaspberryBot", tags: ["Python","ML","Raspberry Pi","Tkinter"], text: "A self learning robot that uses q-learning, a type of Machine Learning, to follow the line"},
+    {title: "PyBlaster", image: pyblaster, link: "https://github.com/denczo/pyblaster", tags: ["Python","PyAudio","Tkinter","RtMidi"], text: "A monophonic synthesizer built with Python that can be played with a Midi keyboard"},
+    {title: "Arc Reactor", image: arc, link: "https://github.com/denczo/arcreactor", tags: ["3D Printing","Blender","C","Arduino"], text: "Using free software like Blender and Cura to model and 3D-print the famous Arc reactor"},
 ]
 export default data;
 
